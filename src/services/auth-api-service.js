6881d4a2fa544e0513da0fd0782f64e34015fcdd
Sam Parkinson <parkinsonsp42@gmail.com>
2019-07-08 16:08:03 +0000

    Add user registration

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,6 +1,6 @@
 import config from '../config'
 
-const AuthApiService ={
+const AuthApiService = {
   postLogin(credentials) {
     return fetch(`${config.API_ENDPOINT}/auth/login`, {
       method: 'POST',
@@ -15,6 +15,20 @@ const AuthApiService ={
           : res.json()
       )
   },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+        ) 
+  }
 }
 
 export default AuthApiService
